38eb08c34a904628cedac06b6f8dd29dadcce158
thang1224 <thanguk04@gmail.com>
2022-08-16 02:40:08 -0700

    prepare change db to mongo

--- Models/Entity/User.cs
+++ Models/Entity/User.cs
@@ -1,18 +1,30 @@
-﻿namespace tiki_shop.Models.Entity
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace tiki_shop.Models.Entity
 {
     public class User
     {
-        public string Id { get; set; }
-        public string Fullname { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("fullName")]
+        public string? Fullname { get; set; }
+        [BsonElement("phoneNumber")]
+        public string? PhoneNumber { get; set; }
+        [BsonElement("password")]
+        public string? Password { get; set; }
+        [BsonElement("balance")]
         public float Balance { get; set; }
-        public float Commission { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
+        [BsonElement("email")]
+        public string? Email { get; set; }
+        [BsonElement("address")]
+        public string? Address { get; set; }
+        [BsonElement("status")]
         public bool Status { get; set; }
-        public int RoleId { get; set; }
-        public Role Role { get; set; }
+        [BsonElement("roleId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? RoleId { get; set; }
         public ICollection<Recharge> Recharges { get; set; }
     }
 }
